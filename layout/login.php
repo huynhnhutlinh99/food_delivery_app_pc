092a7abdc5f53484659fb080968ed7c0d58b9f8b
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-11-18 09:12:11 +0700

    add login design

---
+++ layout/login.php
@@ -0,0 +1,22 @@
+<?php include_once("../layout/includes/head.meta.php")?>
+    <div class="nl-container">
+        <div class="nl-ctn-login">
+            <div class="nl-head">
+                <span class="nl-title">LOGIN</span>
+                <img src="../public/img/icon_x_mark.svg" alt="" class="nl-close">
+            </div>
+            <form action="#">
+                <div class="nl-row nl-a-center nl-mg-b-16">
+                    <label for="#" class="nl-text-lbl">Username</label>
+                    <input type="text" class="nl-ip">
+                </div>
+                <div class="nl-row nl-a-center">
+                    <label for="#" class="nl-text-lbl">Passwork</label>
+                    <input type="text" class="nl-ip">
+                </div>
+
+                <button class="nl-btn-login nl-max">Login</button>
+            </form>
+        </div>
+    </div>
+<?php include_once("../layout/includes/bottom.php")?>
